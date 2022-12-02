2f691b93b365980bb46190be7dab0f145283b5ea
Paul Horikx <p.horikx@gmail.com>
2022-12-02 09:40:27 +0100

    rubocop fixes

--- day1/day_1.rb
+++ day1/day_1.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 require 'pry'
 
-file = File.open("input.txt")
+file = File.open('input.txt')
 file_data = file.read
 elves = file_data.split("\n\n")
 elves_with_numbers = elves.map do |elf|
-  elf.split("\n").map{ |number| number.to_i }
+  elf.split("\n").map(&:to_i)
 end
 
 elves_with_numbers = elves_with_numbers.map(&:sum).sort.reverse
 
+puts elves_with_numbers[0]
 puts elves_with_numbers[0..2].sum
