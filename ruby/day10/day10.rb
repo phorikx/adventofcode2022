84e5245087a296665ef2f87e8a793131b17605e5
Paul Horikx <p.horikx@gmail.com>
2022-12-12 00:00:33 +0100

    finished day 10

--- ruby/day10/day10.rb
+++ ruby/day10/day10.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 class Day10
   attr_accessor :positions
@@ -5,7 +6,7 @@ class Day10
   def initialize
     @file = File.open('input.txt').read.split("\n")
     @register = 1
-    @values = [1,1]
+    @values = [1, 1]
   end
 
   def run_lines
@@ -15,8 +16,8 @@ class Day10
         @values << @register
       when /addx (-?)(\d*)/
         @values << @register
-        value = $2.to_i
-        @register = $1== "" ?  @register + value : @register - value
+        value = Regexp.last_match(2).to_i
+        @register = Regexp.last_match(1) == '' ? @register + value : @register - value
         @values << @register
       end
     end
@@ -26,20 +27,33 @@ class Day10
     @sum = 0
     k = 20
     while k < @values.length
-      @sum += k*@values[k]
+      @sum += k * @values[k]
       puts @sum
-      k+=40
+      k += 40
     end
   end
 
   def print_answer
-    puts @values.length
-    puts @values.inspect
     puts @sum
+    print_screen
+  end
+
+  def print_screen
+    (0..5).each do |i|
+      (1..40).each do |k|
+        register_value = @values[k + 40 * i]
+        if (k - 1 - register_value).abs <= 1
+          print('#')
+        else
+          print(' ')
+        end
+      end
+      print("\n")
+    end
   end
 end
 
 exercise = Day10.new
 exercise.run_lines
 exercise.calculate_answer
-exercise.print_answer 
+exercise.print_answer
